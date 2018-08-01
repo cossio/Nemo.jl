ea3194a9fbe899c051a6dc65e565c028819c9c83
William Hart <goodwillhart@googlemail.com>
2018-08-01 10:43:58 +0200

    Fix a documentation issue for nullspace_right_rational.

--- src/flint/fmpz_mat.jl
+++ src/flint/fmpz_mat.jl
@@ -850,9 +850,9 @@ end
 
 doc"""
     nullspace_right_rational(x::fmpz_mat)
-> Return the right rational nullspace of $x$, i.e. a set of vectors over
-> $\mathbb{Z}$ giving a $\mathbb{Q}$-basis for the nullspace of $x$
-> considered as a matrix over $\mathbb{Q}$.
+> Return a tuple $(r, U)$ consisting of the rank $r$ and $U$, the right rational
+> nullspace of $x$, i.e. a set of vectors over $\mathbb{Z}$ giving a $\mathbb{Q}$-basis 
+> for the nullspace of $x$ considered as a matrix over $\mathbb{Q}$.
 """
 function nullspace_right_rational(x::fmpz_mat)
    z = similar(x)
